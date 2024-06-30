8c79115685c822540c28d4fd6f58e9e2d4a1e127
jHelpinstill <jakob.helpinstill@gmail.com>
2024-06-30 16:55:02 -0500

    Add newRand() function to BlockType

--- BlockType.h
+++ BlockType.h
@@ -22,6 +22,18 @@ inline std::string getBlockName(BlockType b)
 	return "";
 }
 
+inline int newRand(int rand_num)
+{
+	unsigned int a = rand_num;
+	a = (a ^ 61) ^ (a >> 16);
+	a = a + (a << 3);
+	a = a ^ (a >> 4);
+	a = a * 0x27d4eb2d;
+	a = a ^ (a >> 15);
+	a &= ~(1 << 31);
+	return (int)a;
+}
+
 inline float uDist(int rand_num)
 {
 	return (rand_num != RAND_MAX) ? (rand_num / (float)(RAND_MAX)) : (--rand_num / (float)(RAND_MAX));
@@ -59,12 +71,16 @@ inline glm::vec3 getBlockColor(BlockType block, int face, int rand_num = -1)
 	}
 	case BlockType::STONE:
 	{
-		//rand_num = (rand_num << 1) ^ rand_num;
 		rand_num = rand_num / (RAND_MAX / 3);
 		if (rand_num > 1)
 			rand_num = 1;
 		float color = (float)(rand_num + 4) / 7.0f;
 		return glm::vec3(color, color, color);
+
+		// Clown Vomit //
+		//int rand_g = newRand(rand_num);
+		//int rand_b = newRand(rand_g);
+		//return glm::vec3(uDist(rand_num), uDist(rand_g), uDist(rand_b));
 	}
 	}
 	return glm::vec3(1, 0, 1);
